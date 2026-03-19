96fbd46ce4092b6cd492f24052d82423f5d76362
Alex <ajgilroy05@gmail.com>
2026-03-19 17:48:17 +0000

    fixed ui for Grid and Forge
    UI now pops up at the bottom

--- src/CraftingRecipes.cs
+++ src/CraftingRecipes.cs
@@ -4,10 +4,10 @@ using System;
 [GlobalClass]
 public partial class CraftingRecipes : Resource
 {
-    [Export] public string recipeName;
-    [Export] public string[] requiredItemNames;
-    [Export] public string ouputName;
-    [Export] public PackedScene ouputScene;
-    [Export] public string ouputGroup = "";
+	[Export] public string recipeName;
+	[Export] public string[] requiredItemNames;
+	[Export] public string ouputName;
+	[Export] public PackedScene ouputScene;
+	[Export] public string ouputGroup = "";
 
 }
