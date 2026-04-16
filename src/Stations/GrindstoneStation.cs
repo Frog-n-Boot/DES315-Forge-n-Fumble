18b98ba6c70953aa00fb2d34dd878da8a2f4cc7f
Alex <ajgilroy05@gmail.com>
2026-04-16 15:13:40 +0100

    work on polishing more UI and some of the turotial

--- src/Stations/GrindstoneStation.cs
+++ src/Stations/GrindstoneStation.cs
@@ -96,28 +96,6 @@ public partial class GrindstoneStation : BaseStationScript
 		pendingRecipe = null;
 	}
 
-	protected override void OnInputBodyEntered(Node3D body)
-	{
-		if (body.IsInGroup("Player"))
-		{
-			buttonTexture.Visible = true;
-			var p= body as PlayerController;
-			itemCarrier = p as ItemCarrier;
-			player = p;
-			playersInZone.Add(p);
-			p.SetCurrentStation(this);
-		}
-	}
-
-	protected override void OnInputBodyExited(Node3D body)
-	{
-		if(body.IsInGroup("Player")){
-			buttonTexture.Visible = false;
-			var p = body as PlayerController;
-			playersInZone.Remove(p);
-			p?.SetCurrentStation(null);
-		}
-	}
 
 	public float GetCraftDuration()=> craftDuration;
 	public override BarMinigame GetBarMinigame() => barMinigame;
