3b44f451d3710146bfbf824211fc36b49e1a18c9
CRISTIAN RASCANU <2403380@uad.ac.uk>
2026-04-22 10:07:12 +0100

    Audio Manager Implementation & Bug fixes
     Implemented the audio manager to add a fade in/fade out effect to music.

--- src/LoadingMenu.cs
+++ src/LoadingMenu.cs
@@ -1,12 +1,13 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
 public partial class LoadingMenu : Node2D
 {
 	[Export] private string scenePath;
 	[Export] private TextureProgressBar textureProgressBar;
-	[Export] private PackedScene levelScene;
-	
+	//[Export] private PackedScene levelScene;
+	private bool transfitioning = false;
 	private bool sceneLoading =false;
 	// Called when the node enters the scene tree for the first time.
 
@@ -20,21 +21,21 @@ public partial class LoadingMenu : Node2D
 	{
 		if (sceneLoading)
 		{
-			Godot.Collections.Array progress = [];
+			Godot.Collections.Array progress = new Godot.Collections.Array();
 
 			
             var status = ResourceLoader.LoadThreadedGetStatus(scenePath, progress);
        		textureProgressBar.Value = (float)progress[0] * 100;
 
 	 		
-			if(status == ResourceLoader.ThreadLoadStatus.Loaded)
-			{		
-				GetTree().ChangeSceneToPacked(levelScene);
+			if(status == ResourceLoader.ThreadLoadStatus.Loaded && !transfitioning)
+			{	
+				transfitioning = true;
+				//await ToSignal(GetTree().CreateTimer(3.0f), "timeout");
+				var loadedScene = (PackedScene)ResourceLoader.LoadThreadedGet(scenePath);
+				GetTree().ChangeSceneToPacked(loadedScene);
 				sceneLoading = false;
 			}
-		}
-		
-	   
-	   
+		}   
 	}
 }
