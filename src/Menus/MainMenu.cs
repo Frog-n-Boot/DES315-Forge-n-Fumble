becee19ba769d9cb6e2d6d3e176fb1a8e6629886
Cale <calewelsh19@gmail.com>
2026-04-23 11:06:14 +0100

    Main Menu UI Changes

--- src/Menus/MainMenu.cs
+++ src/Menus/MainMenu.cs
@@ -16,6 +16,11 @@ public partial class MainMenu : Control
 	{
 		AudioManager.instance.PlayMusic(menuMusic);
 		startButton.GrabFocus();
+		startButton.Modulate = new Color(0.5f, 0.5f, 0.5f, 1);
+		tutorialButton.Modulate = new Color(0.5f, 0.5f, 0.5f, 1);
+		settingsButton.Modulate = new Color(0.5f, 0.5f, 0.5f, 1);
+		quitMenu.Modulate = new Color(0.5f, 0.5f, 0.5f, 1);
+		
 	}
 
 	public void OnStartButtonPressed(){
@@ -48,6 +53,38 @@ public partial class MainMenu : Control
 			SceneManager.instance.QuitGame();
 		};
 	} 
+	public void OnStartButtonMouseEntered()
+	{
+		startButton.Modulate = new Color(1, 1, 1, 1);
+	}
+	public void OnStartButtonMouseExited()
+	{
+		startButton.Modulate = new Color(0.5f, 0.5f, 0.5f, 1);
+	}
+	public void OnTutorialButtonMouseEntered()
+	{
+		tutorialButton.Modulate = new Color(1, 1, 1, 1);
+	}
+	public void OnTutorialButtonMouseExited()
+	{
+		tutorialButton.Modulate = new Color(0.5f, 0.5f, 0.5f, 1);
+	}
+	public void OnSettingsButtonMouseEntered()
+	{
+		settingsButton.Modulate = new Color(1, 1, 1, 1);
+	}
+	public void OnSettingsButtonMouseExited()
+	{
+		settingsButton.Modulate = new Color(0.5f, 0.5f, 0.5f, 1);
+	}
+	public void OnQuitButtonMouseEntered()
+	{
+		quitMenu.Modulate = new Color(1, 1, 1, 1);
+	}
+	public void OnQuitButtonMouseExited()
+	{
+		quitMenu.Modulate = new Color(0.5f, 0.5f, 0.5f, 1);
+	}
 
 
 }
