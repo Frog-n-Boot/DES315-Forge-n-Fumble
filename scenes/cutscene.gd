233dce5b787641fec7cf56235dd17726a78eb430
SyntaxedAI <softlurker9@gmail.com>
2026-04-29 04:47:53 +0100

    Added cutscene

--- scenes/cutscene.gd
+++ scenes/cutscene.gd
@@ -3,6 +3,13 @@ extends Node2D
 var holdTimer = 0.0
 var holdDuration = 1.0
 
+func _ready():
+	$VideoStreamPlayer.finished.connect(onVideoFinished)
+	$VideoStreamPlayer.play()
+	
+func onVideoFinished():
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("skip_tutorial"):
